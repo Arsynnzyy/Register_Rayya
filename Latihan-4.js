e3a52110a22f374f1e41d201d643d033a1779282
Arsynnzyy <r4yy41004@gmail.com>
2025-08-21 11:19:24 +0700

    Latihan-4.js

--- Latihan-4.js
+++ Latihan-4.js
@@ -1,17 +1,36 @@
-document.getElementById("registerform").addEventListener("submit", function(e){
+document.getElementById("registerform").addEventListener("submit", function(e) {
             e.preventDefault();
+
             const nama = document.getElementById("nama").value;
             const email = document.getElementById("email").value;
-            const phone_number = document.getElementById("phone_number").value;
-            const tanggal_lahir = document.getElementById("tanggal_lahir").value;
-
-            const table = document.getElementById("dataTable").getElementsByTagName("tbody")[0];
+            const password = document.getElementById("password").value;
+            const nomorHP = document.getElementById("nomorHP").value;
+            const tanggal = document.getElementById("tanggal").value;
+            const gender = document.querySelector('input[name="gender"]:checked')?.value || '';
+            
+            // buat simpan di local storage
+            const user = {
+                nama: nama,
+                email: email,
+                password: password,
+                nomorHP: nomorHP,
+                tanggal: tanggal,
+                gender: gender,
+            };
+            localStorage.setItem(enail, JSON.stringify(user));
+            
+            alert("Sukses yah");
+            
+            // Masukan data ke tabel
+            const table = document.getElementById("dataTable").querySelector("tbody");
             const newRow = table.insertRow();
 
             newRow.insertCell(0).textContent = nama;
             newRow.insertCell(1).textContent = email;
-            newRow.insertCell(2).textContent = phone_number;
-            newRow.insertCell(3).textContent = tanggal_lahir;
+            newRow.insertCell(1).textContent = password;
+            newRow.insertCell(2).textContent = nomorHP;
+            newRow.insertCell(3).textContent = tanggal;
+            newRow.insertCell(4).textContent = gender;
 
-            document.getElementById("registerform").reset();
+            form.reset();
         });
